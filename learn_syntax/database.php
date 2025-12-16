827ea174571d4bf8641e3940b338069a4cf1d419
jayamisulakshi <jayamisulakshi@gmail.com>
2025-12-16 19:07:38 +0530

    database connection check

---
+++ learn_syntax/database.php
@@ -0,0 +1,40 @@
+<?php
+
+    $db_server = "localhost";
+    $db_user = "root";
+    $db_pass ="";
+    $db_name = "businessdb";
+    $conn = "";
+
+    
+    
+    try{
+        $conn = mysqli_connect($db_server,
+                                $db_user,
+                                $db_pass, 
+                                $db_name);
+    }
+    catch(mysqli_sql_exception) {
+        echo "Failed to connect.";
+    }
+
+    if($conn){
+        echo "Connected successfully";
+    } 
+    else{
+        echo "Connection failed: ";
+    }
+
+    // $conn = mysqli_connect($db_server,
+    //                         $db_user,
+    //                         $db_pass, 
+    //                         $db_name);
+
+    // if($conn){
+    //     echo "Connected successfully";
+    // }
+    // else{
+    //     echo "Connection failed: ";
+    // }
+
+?>
